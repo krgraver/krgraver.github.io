9e2ee3f476d0a3343d7c75bbd43c52614491354a
krgraver <krgraver@gmail.com>
2015-10-18 16:09:55 -0500

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     $('#headerBG').css('top',(0-(scrolled*.75))+'px');
 }
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
