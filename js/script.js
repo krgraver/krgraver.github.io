cc288800c3a7e7ba7e4bb3b138290cb848519c2d
krgraver <krgraver@gmail.com>
2015-11-25 00:13:59 -0600

    changed everything

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 // JavaScript Document
 
-$(window).bind('scroll',function(e){
-    parallaxScroll();
-});
-
-function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    $('#headerBG').css('top',(0-(scrolled*.75))+'px');
-}
-
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -23,3 +14,63 @@ $(document).ready(function(){
 	    });
 	});
 });
+
+$(document).ready(function () {
+	$(document).on("scroll", onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		$('a').each(function () {
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+
+		var target = this.hash;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top+2
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+});
+
+function onScroll(event){
+	var scrollPosition = $(document).scrollTop();
+	$('nav a').each(function () {
+		var currentLink = $(this);
+		var refElement = $(currentLink.attr("href"));
+		if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+			$('nav ul li a').removeClass("active");
+			currentLink.addClass("active");
+		}
+		else{
+			currentLink.removeClass("active");
+		}
+	});
+}
+
+$(function() {
+	$("#navContent a").click(function(){
+		$("#navContent a").removeClass("active");
+		$(this).addClass("active");
+	});
+});
+
+$("#arrow img")
+	.delay(8000)
+	.animate({opacity: ".6"}, {duration: 1000, queue: true});
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#headshot').css('top',(0-(scrolled*.3))+'px');
+}
+
+
