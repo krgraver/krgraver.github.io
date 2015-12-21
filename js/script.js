e94db9dbc17d7edb0a22b94e9b34d0e6d36304e1
krgraver <krgraver@gmail.com>
2015-12-21 03:05:58 -0600

    added two case studies

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 
 $(document).ready(function () {
-	$(document).on("scroll", onScroll);
+	$(document).on('scroll', onScroll);
 
 	$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
-		$(document).off("scroll");
+		$(document).off('scroll');
 
 		$('a').each(function () {
 			$(this).removeClass('active');
@@ -18,7 +18,7 @@ $(document).ready(function () {
 			'scrollTop': $target.offset().top+2
 		}, 500, 'swing', function () {
 			window.location.hash = target;
-			$(document).on("scroll", onScroll);
+			$(document).on('scroll', onScroll);
 		});
 	});
 });
@@ -27,37 +27,48 @@ function onScroll(event){
 	var scrollPosition = $(document).scrollTop();
 	$('nav a').each(function () {
 		var currentLink = $(this);
-		var refElement = $(currentLink.attr("href"));
+		var refElement = $(currentLink.attr('href'));
 		if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-			$('nav ul li a').removeClass("active");
-			currentLink.addClass("active");
+			$('nav ul li a').removeClass('active');
+			currentLink.addClass('active');
 		}
 		else{
-			currentLink.removeClass("active");
+			currentLink.removeClass('active');
 		}
 	});
 }
 
 $(window).scroll(function() {
-	if ($('body').scrollTop() > 10 && $(window).width() > 870) {
+	if ($('body').scrollTop() > 60 && $(window).width() > 870) {
 		$('#navContent .logo').addClass('scroll');
 		$('#navContent ul').addClass('scroll');
 	} else {
 		$('#navContent .logo').removeClass('scroll');
 		$('#navContent ul').removeClass('scroll');
 	}
+
+	if ($('body').scrollTop() > 60) {
+		$('nav').addClass('scroll');
+	} else {
+		$('nav').removeClass('scroll');
+	}
 });
 
 $(function() {
-	$("#navContent a").click(function(){
-		$("#navContent a").removeClass("active");
+	$('#navContent a').click(function(){
+		$('#navContent a').removeClass('active');
 		$(this).addClass("active");
 	});
 });
 
-$("#arrow img")
+$('#arrow img')
 	.delay(8000)
 	.animate({opacity: .6}, {duration: 1000, queue: true});
+
+$('#topBtn').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({scrollTop: 0}, 1000);
+});
 
 
 
